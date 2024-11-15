44ec06c7c3d27f8356d94b4ad1722762d61e0ed5
gtorresluiz <rm98600@fiap.com.br>
2024-11-14 21:47:35 -0300

    membros

---
+++ EcoScene.py
@@ -0,0 +1,12 @@
+#membros
+grupo = {
+    1: {
+        "Nome": "Eduardo Fedeli Souza", "RM": 550132
+    },
+    2 : {
+    "Nome": "Gabriel Torres Luiz", "RM": 98600
+    },
+    3 : {
+        "Nome": "Otávio Vitoriano da Silva", "RM": 552012
+    }
+}
